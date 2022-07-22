55a35c366e628dc58248b7bf0c24c07b051ecfb1
sanjay0001 <75019244+sanjay0001@users.noreply.github.com>
2022-07-22 18:30:30 +0530

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mychat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ Path(BASE_DIR, 'templates') ],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
